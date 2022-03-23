09df14bd76ef41f7f1d0be7071027b7b3a39faa8
Dmitriy <home@gmail.com>
2022-03-23 13:44:07 +0300

    доб сериализацию

--- ConsoleClient/Message.cs
+++ ConsoleClient/Message.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace MyMessanger
 {
+    [Serializable]
     public class Message
-    {
+    {        
         public string UserName { get; set; }
         public string MessageText { get; set; }
         public DateTime TimeStamp { get; set; }
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,6 +1,12 @@
 ﻿using MyMessanger;
+using Newtonsoft.Json;
 
+Message msg = new Message("Dmitriy", "Privet!", DateTime.UtcNow);
+string output = JsonConvert.SerializeObject(msg);
+Console.WriteLine(output);
+Message deserializeMsg = JsonConvert.DeserializeObject<Message>(output);
+Console.WriteLine(deserializeMsg);
+//{ "UserName":"Dmitriy","MessageText":"Privet!","TimeStamp":"2022-03-23T10:40:34.0101235Z"}
+//Dmitriy < 23.03.2022 10:40:34 >: Privet!
 
-Message msg = new Message();
-Console.WriteLine(msg.ToString());
 
